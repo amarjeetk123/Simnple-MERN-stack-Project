754a5efa7c1e0c7ef86e0854a16316ad7a756090
amarjeetk123 <ak123del@gmail.com>
2022-11-21 22:23:31 +0530

    initial commit

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -62,7 +62,7 @@ exports.editUser = async (req, res) =>{
         const user = await User.findByIdAndUpdate(req.params.id , req.body)
         res.status(200).json({
             success : true ,
-            message : "user update succesfully"
+            message : "user update succesfull"
         })
         
     } catch (error) {
